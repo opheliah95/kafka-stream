c6aed5894b40227874c5980ada0c3a0eb95ebe7b
opheliah95 <opheliah95@gmail.com>
2022-05-06 09:53:29 +0100

    set up kafka consumer

--- src/main/java/SampleConsumer.java
+++ src/main/java/SampleConsumer.java
@@ -1,5 +1,10 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import java.util.Properties;
-
+import java.util.Arrays;
 import static com.helper.github.opheliah95.MyConst.*;
 
 public class SampleConsumer {
@@ -8,6 +13,22 @@ public class SampleConsumer {
         Properties prop = new Properties();
         prop.put(SERVER, HOST);
         prop.put("group.id", GROUP_ID);
+        prop.put("enable.auto.commit", AUTO_COMMIT);
+        prop.put("auto.commit.interval.as", COMMIT_INTERVAL);
+        prop.put("key.deserializer", StringDeserializer.class.getName());
+        prop.put("value.deserializer", StringDeserializer.class.getName());
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(prop);
+        consumer.subscribe(Arrays.asList(TOPIC_LIST));
+        // consumer record
+        while(true){
+            ConsumerRecords<String, String> records = consumer.poll(1000);
+            for (ConsumerRecord<String, String> record: records){
+                System.out.printf("offset=%d, key=%s, value=%s\n", record.offset(), record.key(), record.value());
+            }
+
+        }
+
     }
+
 
 }
--- src/main/java/com/helper/github/opheliah95/MyConst.java
+++ src/main/java/com/helper/github/opheliah95/MyConst.java
@@ -6,4 +6,10 @@ public class MyConst {
     public static final String SERVER = "bootstrap.servers";
     public static final String HOST = "localhost:9092";
     public static final String GROUP_ID = "group1";
+
+    // commits
+    public static final String AUTO_COMMIT="true";
+    public static final String COMMIT_INTERVAL = "1000";
+
+    public static final String[] TOPIC_LIST = new String[]{"hello-kafka"};
 }
